fb8dc38fc3117f0115408efc4e223cdb5f842e75
Arjun Tanwar <arjunthakur900@gmail.com>
2022-12-15 09:49:43 +0530

    fixing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Splash from './components/Splash';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './components/home/Home';
+import { AiFillGithub } from 'react-icons/ai';
 import { DefaultEventsMap } from 'socket.io/dist/typed-events'
 import { Socket } from 'socket.io-client';
 import { GiHamburgerMenu } from 'react-icons/gi'
@@ -14,14 +15,19 @@ function App() {
   const [hideNav, setHideNav] = React.useState<boolean>(true)
   return (
     <div className = 'w-screen relative font-nunito h-screen'>
-      <nav className = 'w-full flex fixed shadow-sm shadow-gray-600 top-0 left-0 bg-teal-800 text-white items-center gap-1 z-20 pl-3 py-1'>
-        <GiHamburgerMenu onClick = {() => {
-          setHideNav(i => !i)
-        }} className = {`md:hidden cursor-pointer text-xl mr-2 ${pathname !== '/'? '': 'cursor-not-allowed'}`} />
-        <div className = 'text-lg'>
-          <IoChatbubblesOutline/>
+      <nav className = 'w-full flex fixed shadow-sm shadow-gray-600 top-0 left-0 bg-teal-800 text-white items-center justify-between gap-1 z-20 pl-3 py-1'>
+        <div className='flex justify-start items-center gap-1'>
+          <GiHamburgerMenu onClick = {() => {
+            setHideNav(i => !i)
+          }} className = {`md:hidden cursor-pointer text-xl mr-2 ${pathname !== '/'? '': 'cursor-not-allowed'}`} />
+          <div className = 'text-lg'>
+            <IoChatbubblesOutline/>
+          </div>
+          <div>
+            IntelliChat
+          </div>
         </div>
-        IntelliChat
+        <AiFillGithub className = 'text-white text-2xl mr-2' />
       </nav>
       <Routes>
         <Route path='/' element={<Splash setSocket = {setSocket}/>} />
